420be5b6ec8b1d35331277a9c7c1e45915bdfd14
Vordenby <ceo65-1@outlook.com>
2026-04-20 00:10:45 +0300

    Last task is done

--- Practice-8/ПР8_ЯковлевВиктор_бИПТ-251.py
+++ Practice-8/ПР8_ЯковлевВиктор_бИПТ-251.py
@@ -84,3 +84,55 @@ else:
 
 print("Вы можете к нам поступить!" if ok else "Увы...")
 
+# Задание 5
+
+class NoMoneyToWithdrawError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class PaymentError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def print_accounts(accounts):
+    """Печать аккаунтов."""
+    print("Список клиентов ({}): ".format(len(accounts)))
+    for i, (name, value) in enumerate(accounts.items(), start=1):
+        print("{}. {} {}".format(i, name, value))
+
+
+def transfer_money(accounts, account_from, account_to, value):
+    if account_from not in accounts or account_to not in accounts:
+        raise PaymentError("Один из аккаунтов не найден")
+    
+    if accounts[account_from] < value:
+        raise NoMoneyToWithdrawError("Недостаточно средств для перевода")
+    
+    accounts[account_from] -= value
+    accounts[account_to] += value
+
+ 
+if __name__ == "__main__":
+    accounts = {
+        "Василий Иванов": 100,
+        "Екатерина Белых": 1500,
+        "Михаил Лермонтов": 400
+    }
+    print_accounts(accounts)
+
+    payment_info = {
+        "account_from": "Екатерина Белых",
+        "account_to": "Василий Иванов"
+    }
+
+    print("Перевод от {account_from} для {account_to}...".format(**payment_info))
+
+    payment_info["value"] = int(input("Сумма = "))
+
+    transfer_money(accounts, **payment_info)
+
+    print("OK!")
+
+    print_accounts(accounts)
