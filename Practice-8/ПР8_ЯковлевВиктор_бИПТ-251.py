4f26fc59f57cfd148bc3f4daf313b6e0b1ec939b
Vordenby <ceo65-1@outlook.com>
2026-04-20 00:10:08 +0300

    Third task is done

--- Practice-8/ПР8_ЯковлевВиктор_бИПТ-251.py
+++ Practice-8/ПР8_ЯковлевВиктор_бИПТ-251.py
@@ -33,3 +33,18 @@ def power(x, y=2):
         return 1
     else:
         return x * power(x, y - 1)
+
+# Задание 3
+
+n = int(input("Введите кол-во человек: "))
+
+middle_names = {}
+for i in range(n):
+    fio = input("Введите ФИО через пробел: ").split()
+
+    middle_name = fio[2]
+    middle_names[middle_name] = middle_names.get(middle_name, 0) + 1
+
+print(sorted(middle_names.items(), key=lambda item: item[1])[-1][0])
+print("В расчете участвовало человек:", n)
+
