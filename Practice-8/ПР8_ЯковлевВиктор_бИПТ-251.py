e6c49ad5d5a2d8d4862ca5ba11f2f0b4c8d8f797
Vordenby <ceo65-1@outlook.com>
2026-04-20 00:09:20 +0300

    First task is done

---
+++ Practice-8/ПР8_ЯковлевВиктор_бИПТ-251.py
@@ -0,0 +1,29 @@
+
+# Задание 1
+def ceasar(text, shift):
+    """Вернуть измененную строку 'text' со сдвигом 'shift'.
+
+    Параметры:
+        - text (str): строка;
+        - shift (int): сдвиг.
+
+    Результат:
+        str: измененная строка."""
+    
+    letters = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+
+    result = ""
+    for char in text:
+        if char.lower() in letters:
+            index = letters.index(char.lower())
+            new_index = (index + shift) % len(letters)
+            if char.isupper():
+                result += letters[new_index].upper()
+            else:
+                result += letters[new_index]
+        else:
+            result += char
+    
+    return result
+
+
