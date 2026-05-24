5df448508bd06107d0a3aa3c67103f415c4e276d
Vordenby <ceo65-1@outlook.com>
2026-05-24 06:11:40 +0300

    i need a concretic version

--- Practice-10/CallScreen/uix/screens/baseclass/callscreen.py
+++ Practice-10/CallScreen/uix/screens/baseclass/callscreen.py
@@ -2,23 +2,23 @@ from pathlib import Path
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 from kivy.animation import Animation
+from kivy.properties import BooleanProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class ItemList(BoxLayout):
+    icon = StringProperty("")
+    text = StringProperty("")
+    secondary_text = StringProperty("")
 
 HERE = Path(__file__).resolve().parent
 KV_PATH = HERE.parent / "kv" / "callscreen.kv"
-
 Builder.load_file(str(KV_PATH))
 
 class CallScreen(MDScreen):
-    open_call_box = False
+    open_call_box = BooleanProperty(False)
 
     def animation_title_image(self, title_image):
-        """
-        :type title_image: <kivymd.utils.fitimage.FitImage object>
-        """
-
         if not self.open_call_box:
-            Animation(self.size_hint_y=1, d=0.6, t="in_out_quad").start(title_image)
+            Animation(size_hint_y=1, d=0.6, t="in_out_quad").start(title_image)
         else:
-            Animation(self.size_hint_y=0.45, d=0.6, t="in_out_quad").start(title_image)
-    
-    
+            Animation(size_hint_y=0.45, d=0.6, t="in_out_quad").start(title_image)
